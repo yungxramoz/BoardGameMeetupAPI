16a19d121190e3f9cacad762060d9514e13b34c4
Sandro Gerber <sandro-gerber@hispeed.ch>
2020-09-06 12:23:21 +0200

    Create Interface for SecurityContext

---
+++ src/BoardgameMeetup.Security/ISecurityContext.cs
@@ -0,0 +1,10 @@
+﻿using BoardgameMeetup.Data.Models;
+
+namespace BoardgameMeetup.Security
+{
+    public interface ISecurityContext
+    {
+        User User { get; }
+        bool IsAdministrator { get; }
+    }
+}
