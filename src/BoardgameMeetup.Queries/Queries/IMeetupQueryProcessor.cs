824d75b2798ab0687553367fef6c752e628a123f
Sandro Gerber <sandro-gerber@hispeed.ch>
2020-09-06 13:14:15 +0200

    Add initial query processors for meetup

---
+++ src/BoardgameMeetup.Queries/Queries/IMeetupQueryProcessor.cs
@@ -0,0 +1,16 @@
+﻿using BoardgameMeetup.Api.Models.Meetup;
+using BoardgameMeetup.Data.Models;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BoardgameMeetup.Queries.Queries
+{
+    public interface IMeetupQueryProcessor
+    {
+        IQueryable<Meetup> Get();
+        Meetup Get(int id);
+        Task<Meetup> Create(CreateMeetupModel model);
+        Task<Meetup> Update(int id, UpdateMeetupModel model);
+        Task Delete(int id);
+    }
+}
---
+++ src/BoardgameMeetup.Queries/Queries/MeetupQueryProccessor.cs
@@ -0,0 +1,38 @@
+﻿using BoardgameMeetup.Api.Models.Meetup;
+using BoardgameMeetup.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoardgameMeetup.Queries.Queries
+{
+    class MeetupQueryProccessor : IMeetupQueryProcessor
+    {
+        public Task<Meetup> Create(CreateMeetupModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Meetup> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Meetup Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Meetup> Update(int id, UpdateMeetupModel model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
