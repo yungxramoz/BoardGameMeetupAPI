321bee53cb9113fc84fc32fe1b4f85d567cca733
Sandro Gerber <sandro-gerber@hispeed.ch>
2020-09-07 21:55:12 +0200

    Add data result model

---
+++ src/BoardgameMeetup.Api.Models/Common/DataResult.cs
@@ -0,0 +1,14 @@
+﻿namespace BoardgameMeetup.Api.Models.Common
+{
+    public class DataResult<T>
+    {
+        public T[] Data { get; set; }
+        public int Total { get; set; }
+    }
+
+    public class DataResult
+    {
+        public object Data { get; set; }
+        public int Total { get; set; }
+    }
+}
