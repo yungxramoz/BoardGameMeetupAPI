8f9f7fe77b42c0ce57bc01c28c76f95c5ed782bd
Sandro Gerber <sandro-gerber@hispeed.ch>
2020-09-07 19:47:51 +0200

    Add user / usertoken api models

---
+++ src/BoardgameMeetup.Api.Models/User/UserModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BoardgameMeetup.Api.Models.User
+{
+    public class UserModel
+    {
+        public UserModel()
+        {
+            Roles = new string[0];
+        }
+
+        public Guid Id { get; set; }
+        public string Username { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string[] Roles { get; set; }
+    }
+}
---
+++ src/BoardgameMeetup.Api.Models/User/UserWithTokenModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace BoardgameMeetup.Api.Models.User
+{
+    public class UserWithTokenModel
+    {
+        public string Token { get; set; }
+        public UserModel User { get; set; }
+        public DateTime ExpiresAt { get; set; }
+    }
+}
