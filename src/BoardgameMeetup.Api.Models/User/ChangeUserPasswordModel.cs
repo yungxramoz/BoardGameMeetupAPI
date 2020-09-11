1f22c0ba888aec84d3ffda482a46d5ff8d424d99
Sandro Gerber <sandro-gerber@hispeed.ch>
2020-09-11 14:55:42 +0200

    Add all API models for user entity

---
+++ src/BoardgameMeetup.Api.Models/User/ChangeUserPasswordModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BoardgameMeetup.Api.Models.User
+{
+    class ChangeUserPasswordModel
+    {
+        [Required]
+        public string Password { get; set; }
+    }
+}
---
+++ src/BoardgameMeetup.Api.Models/User/UpdateUserModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BoardgameMeetup.Api.Models.User
+{
+    public class UpdateUserModel
+    {
+        public UpdateUserModel()
+        {   
+            Roles = new string[0];
+        }
+
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+
+        public string[] Roles { get; set; }
+    }
+}
