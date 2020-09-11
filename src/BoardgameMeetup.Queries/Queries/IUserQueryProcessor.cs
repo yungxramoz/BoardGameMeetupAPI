4fa3a7578b73402f297bd4f06148c802e4ea34e7
Sandro Gerber <sandro-gerber@hispeed.ch>
2020-09-11 15:01:11 +0200

    Add unimplemented UserQueryProcessor

---
+++ src/BoardgameMeetup.Queries/Queries/IUserQueryProcessor.cs
@@ -0,0 +1,17 @@
+﻿using BoardgameMeetup.Api.Models.User;
+using BoardgameMeetup.Data.Models;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BoardgameMeetup.Queries.Queries
+{
+    public interface IUserQueryProcessor
+    {
+        IQueryable<User> Get();
+        User Get(Guid id);
+        Task<User> Create(CreateUserModel model);
+        Task<User> Update(Guid id, UpdateUserModel model);
+        Task Delete(Guid id);
+    }
+}
---
+++ src/BoardgameMeetup.Queries/Queries/UserQueryProcessor.cs
@@ -0,0 +1,36 @@
+﻿using BoardgameMeetup.Api.Models.User;
+using BoardgameMeetup.Data.Models;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BoardgameMeetup.Queries.Queries
+{
+    public class UserQueryProcessor : IUserQueryProcessor
+    {
+        public Task<User> Create(CreateUserModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<User> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public User Get(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> Update(Guid id, UpdateUserModel model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
